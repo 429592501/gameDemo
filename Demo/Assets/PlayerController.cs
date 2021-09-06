fdbda7b08955aec61274f450ea8a348874c1fa4c
zhangruizhi <429592501qq.com>
2021-09-06 16:24:39 +0800

    提交

--- Demo/Assets/PlayerController.cs
+++ Demo/Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float playerSpeed;
     public Rigidbody2D rb;//获取刚体对象
+    public Animator anim;//获取动画控制器
 
 
     // Start is called before the first frame update
@@ -23,9 +24,19 @@ public class PlayerController : MonoBehaviour
     //角色移动
     void Run() {
         float moveX = Input.GetAxis("Horizontal");
-        
-        if (moveX != 0) {
-            rb.velocity = new Vector2(moveX * playerSpeed, rb.velocity.y );
+        float dirX = Input.GetAxisRaw("Horizontal");
+        if (moveX != 0)
+        {
+            rb.velocity = new Vector2(moveX * playerSpeed, rb.velocity.y);
+            anim.SetBool("runing", true);
+            if (dirX != 0) {
+                transform.localScale = new Vector3(dirX,1,1);
+            }
+          
+        }
+        else 
+        {
+            anim.SetBool("runing", false);
         }
         
     }
