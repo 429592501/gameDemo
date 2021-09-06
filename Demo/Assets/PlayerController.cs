b313f8fcb11eb3022deff3f3e5799f24a6f1ea44
zhangruizhi <429592501qq.com>
2021-09-06 15:45:30 +0800

    提交

---
+++ Demo/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed;
+    public Rigidbody2D rb;//获取刚体对象
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    //角色移动
+    void Run() {
+        float moveX = Input.GetAxis("Horizontal");
+        
+        if (moveX != 0) {
+            rb.velocity = new Vector2(moveX * playerSpeed, rb.velocity.y );
+        }
+        
+    }
+}
